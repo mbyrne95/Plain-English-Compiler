87037afe28070b59709a3155824e6de3e9a60cbc
Matt Byrne <76924518+mbyrne95@users.noreply.github.com>
2026-04-19 21:18:50 -0400

    Update Main.java

--- PE_Skeleton/Main.java
+++ PE_Skeleton/Main.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        String source = new String(Files.readAllBytes(Paths.get("C:/Users/Matt/IdeaProjects/PE_Skeleton/PE_Skeleton/FirstProgram.eng")));
+        String source = new String(Files.readAllBytes(Paths.get("path_to_ple_program")));
         Lexer lexer = new Lexer(source);
         List<Token> tokens = lexer.lex();
         PlainEnglishParser parser = new PlainEnglishParser(new LinkedList<>(tokens));
