572eab537247bc4618916d44bb5526b3158cddc0
Matt Byrne <76924518+mbyrne95@users.noreply.github.com>
2026-04-30 15:51:00 -0400

    update racket program

--- mbyrne_scheme.rkt
+++ mbyrne_scheme.rkt
@@ -1,10 +1,6 @@
-; x^2
-(define (square x)
-  (* x x))
-
 ; (x2 - x1)^2
 (define (subAndSquare x1 x2)
-  (square (- x1 x2)))
+  (* (- x1 x2) (- x1 x2)))
 
 ; sqrt ( (x2 - x1)^2 + (y2 - y1)^2 + (z2 - z1)^2 )
 (define (distance p1 p2)
@@ -16,13 +12,20 @@
 ; if head of list closer than currentClosest, recurse with head of list as new currentClosest
 ; else recurse with same currentClosest
 (define (findClosest p listPoints currentClosest)
-  (if (null? listPoints) currentClosest
-      (if (< (distance p (car listPoints)) (distance p currentClosest))
-          (findClosest p (cdr listPoints) (car listPoints))
-          (findClosest p (cdr listPoints) currentClosest))))
+  (cond
+    ((null? listPoints) currentClosest)
+    ((< (distance p (car listPoints)) (distance p currentClosest))
+     (findClosest p (cdr listPoints) (car listPoints)))
+    (else
+     (findClosest p (cdr listPoints) currentClosest))))
 
+; interface
 (define (findSmallest p l)
   (let ((closest (findClosest p (cdr l) (car l))))
   (list (distance p closest) p closest)))
 
+; tests
 (findSmallest '(12 6 5) '((1 4 3) (7 2 5) (9 3 22) (12 5 9)))
+(findSmallest '(7 2 5) '((1 4 3) (7 2 5) (9 3 22) (12 5 9)))
+(findSmallest '(0 0 0) '((3 4 0)))
+(findSmallest '(0 0 0) '((1 0 0) (0 1 0) (0 0 1)))
